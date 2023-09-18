2e3822c59e6b2593c9f0549683ff1e031256eb80
Weoka <weokap@gmail.com>
2023-09-18 16:20:56 -0500

    [ADD]: Update task

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
 import { Task } from 'src/app/models/task.model';
+import { TaskService } from 'src/app/services/task.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-task',
@@ -8,14 +10,31 @@ import { Task } from 'src/app/models/task.model';
   styles: [
   ]
 })
-export class TaskComponent {
-  @Input() task:Task = {
+export class TaskComponent implements OnInit {
+  constructor(
+    private taskService: TaskService
+  ) {}
+
+  ngOnInit() {
+    this.completed.valueChanges.subscribe(value => {
+      this.localTask.completed = value;
+      this.taskService.updateTask(this.localTask);
+    });
+  }
+
+  localTask:Task = {
     id: '',
     title: '',
     completed: false
+  }
+
+  @Input() set task(task: Task) {
+    this.localTask = task;
+    this.completed.setValue(task.completed);
   };
 
   editing: boolean = false;
+  completed = new FormControl();
 
   edit() {
     this.editing = true;
