a58dd4057e28ff1e175e7ec7199e6e75733cf91a
Weoka <weokap@gmail.com>
2023-09-18 13:08:57 -0500

    [UPDATE]: Hide main and footer when 0 tasks

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/models/task.model';
+import { TaskService } from 'src/app/services/task.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
-  constructor() { }
+  tasks: Task[] = [];
+
+  constructor(
+    private taskService: TaskService
+  ) { }
+
+  ngOnInit() {
+    this.taskService.tasks$.subscribe((tasks: Task[]) => {
+      this.tasks = tasks;
+    });
+  }
 
 }
