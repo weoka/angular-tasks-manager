685e0f8dd6b1065cf0ec28c28d369fa0a9318a2f
Weoka <weokap@gmail.com>
2023-09-18 13:01:11 -0500

    [ADD]: Task service methods

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -7,9 +7,29 @@ import { Task } from 'src/app/models/task.model';
 })
 export class TaskService {
 
+  private localTasks: Task[] = [];
   private tasks = new BehaviorSubject<Task[]>([]);
 
   tasks$ = this.tasks.asObservable();
 
-  constructor() { }
+  addTask(task: Task) {
+    this.localTasks.push(task);
+    this.tasks.next(this.localTasks);
+    window.localStorage.setItem('tasks', JSON.stringify(this.localTasks));
+  }
+
+  loadTasks(): boolean {
+    let savedTasks = window.localStorage.getItem('tasks');
+
+    if(!savedTasks) return false;
+
+    this.localTasks = JSON.parse(savedTasks);
+    this.tasks.next(this.localTasks);
+
+    return true;
+  }
+
+  constructor() {
+    this.loadTasks();
+  }
 }
